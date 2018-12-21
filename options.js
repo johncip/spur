076d68d852d265f23803a06fa3183700887492ab
John Cipriano <johnmikecip@gmail.com>
2018-12-21 02:31:10 -0500

    grow options.html & add quotes inputs

--- options.js
+++ options.js
@@ -1,8 +1,28 @@
+function renderQuote(quote, $root) {
+  const $parent = $('<div class="quoteParent">');
+  const $left = $('<div class="leftChild">').appendTo($parent);
+  const $right = $('<div class="rightChild">').appendTo($parent);
+
+  $('<textarea class="quoteInput">').val(quote['quote']).appendTo($left);
+  $('<input type="text" class="authorInput">').val(quote['author']).appendTo($left);
+  $('<input type="text" class="categoryInput">').val(quote['category']).appendTo($left);
+  $('<input type="text" class="urlInput">').val(quote['url']).appendTo($left);
+
+  $('<button>').text('✖').appendTo($right);
+
+  $parent.appendTo($root);
+}
+
 /* Renders the options page. */
 async function render() {
   // do options
   const opts = await loadOptions();
   $('#wakeTime').val(opts.wakeTime); // TODO: make this a time input
+
+  // do quotes
+  const quotes = await loadQuotes();
+  const $root = $('#quotes');
+  quotes.forEach((quote) => renderQuote(quote, $root));
 }
 
 $('button').click(async () => {
