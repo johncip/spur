821cb93bfac76fa059fd33d2459aae17068acbe5
John Cipriano <johnmikecip@gmail.com>
2019-01-04 01:34:25 -0500

    refactor template string creation

--- options.js
+++ options.js
@@ -8,38 +8,39 @@ const Corpus = Backbone.Collection.extend({
 
 const QuoteListItem = Backbone.View.extend({
   tagName: 'li',
-  renderEdit: function() {
-    return Mustache.render(`
+
+  // TODO: this seems backwards
+  wrapTemplate: function(middle) {
+    return `
       <div class="quoteListItem">
         <div class="quoteListItem--first">
-          <textarea class="edit edit-quote">{{ quote }}</textarea>
-          <input type="text" class="edit edit-author" value="{{author}}"></input>
-          <input type="text" class="edit edit-category" value="{{category}}"></input>
-          <input type="text" class="edit edit-url" value="{{url}}"></input>
+          ${middle}
         </div>
         <div class="quoteListItem--second">
-          <button class="deleteBtn">✖</button>
+          <button class="btn btn-delete">✖</button>
         </div>
       </div>
+    `;
+  },
+  renderEdit: function() {
+    const template = this.wrapTemplate(`
+      <textarea class="edit edit-quote">{{ quote }}</textarea>
+      <input type="text" class="edit edit-author" value="{{author}}"></input>
+      <input type="text" class="edit edit-category" value="{{category}}"></input>
+      <input type="text" class="edit edit-url" value="{{url}}"></input>
     `, this.model.attributes);
+    return Mustache.render(template, this.model.attributes);
   },
   renderDisplay: function() {
-    return Mustache.render(`
-      <div class="quoteListItem">
-        <div class="quoteListItem--first">
-          <div class="display display-quote">{{ quote }}</div>
-          <div class="display display-author">{{ author }}</div>
-          <div class="display display-category">{{ category }}</div>
-          <div class="display display-url">{{ url }}</div>
-        </div>
-        <div class="quoteListItem--second">
-          <button class="deleteBtn">✖</button>
-        </div>
-      </div>
-    `, this.model.attributes);
+    const template = this.wrapTemplate(`
+      <div class="display display-quote">{{ quote }}</div>
+      <div class="display display-author">{{ author }}</div>
+      <div class="display display-category">{{ category }}</div>
+      <div class="display display-url">{{ url }}</div>
+    `);
+    return Mustache.render(template, this.model.attributes);
   },
   render: function() {
-    // const dom = this.renderEdit();
     const dom = this.renderDisplay();
     $(dom).appendTo(this.$el);
     return this;
