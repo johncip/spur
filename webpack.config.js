ad81ea0feacd5f31de9cb16ac36c13c4994ca6c3
John Cipriano <johnmikecip@gmail.com>
2019-02-05 15:26:56 -0800

    add initial webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+
+  entry: {
+    app: './src/app.js',
+    options: './src/options.js',
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      filename: 'app.html',
+      chunks: ['app'],
+      template: './src/app.html',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'options.html',
+      chunks: ['options'],
+      template: './src/options.html',
+    }),
+  ],
+};
