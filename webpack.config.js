66fc4532924ab4ef94d9cf9a9d2f685f80772b25
John Cipriano <johnmikecip@gmail.com>
2019-02-05 16:10:06 -0800

    clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -24,11 +24,7 @@ module.exports = {
   module: {
     rules: [{
       test: /\.scss$/,
-      use: [
-        "style-loader",
-        "css-loader",
-        "sass-loader",
-      ],
+      use: ['style-loader', 'css-loader', 'sass-loader'],
     }],
   },
 
