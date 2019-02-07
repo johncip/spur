f27dbf15f8577737db2a382cf33e88af030db181
John Cipriano <johnmikecip@gmail.com>
2019-02-06 16:05:16 -0800

    comment out web-ext plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const WebpackWebExt = require('webpack-webext-plugin');
+// const WebpackWebExt = require('webpack-webext-plugin');
 
 module.exports = {
   mode: 'production',
