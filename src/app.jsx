d6780a726c62788cfc58d332a61709e0fbd74072
John Cipriano <johnmikecip@gmail.com>
2019-02-08 20:04:57 -0800

    delint app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 import ReactDOM from 'react-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faBox } from '@fortawesome/free-solid-svg-icons/faBox';
 import { faGlobeAmericas } from '@fortawesome/free-solid-svg-icons/faGlobeAmericas';
 
@@ -15,10 +14,10 @@ import 'Styles/index_card_dark.scss';
 /*
  * A button captioned with an up or down triangle.
  */
-class VerticalToggle extends Component {
+class VerticalToggle extends PureComponent {
   render() {
     return (
-      <button id="toggle" onClick={this.props.handleClick}>
+      <button id="toggle" onClick={this.props.handleClick} type="button">
         {this.props.up ? '\u25b2' : '\u25bc'}
       </button>
     );
@@ -29,6 +28,15 @@ class VerticalToggle extends Component {
  * A displayed quote, with author, etc.
  */
 class Quote extends Component {
+  /**
+   * Returns an appropriate font size for the given string. That is, longer strings will
+   * return a smaller size, and shorter strings will return a larger size.
+   */
+  static adjustedFontSize(text) {
+    const size = 150 * (1 / (text.length ** 0.3));
+    return `${size}px`;
+  }
+
   constructor(props) {
     super(props);
 
@@ -41,16 +49,7 @@ class Quote extends Component {
    * Reverses state.expanded (which hides & shows the URL and category).
    */
   toggleDrawer = () => {
-    this.setState({expanded: !this.state.expanded});
-  }
-
-  /**
-   * Returns an appropriate font size for the given text string. That is, longer strings will
-   * have a smaller font size, and shorter strings will have a larger font size.
-   */
-  adjustedFontSize(text) {
-    const size = 150 * (1 / (text.length ** 0.3));
-    return `${size}px`;
+    this.setState(prev => ({ expanded: !prev.expanded }));
   }
 
   containerClass() {
@@ -61,13 +60,15 @@ class Quote extends Component {
   }
 
   render() {
+    const quoteFontSize = this.constructor.adjustedFontSize(this.props.quote);
+
     return (
       <div id="container" className={this.containerClass()}>
-        <div id="quote" style={{fontSize: this.adjustedFontSize(this.props.quote)}}>
+        <div id="quote" style={{ fontSize: quoteFontSize }}>
           {this.props.quote}
         </div>
         <div id="author">{this.props.author}</div>
-        <hr/>
+        <hr />
         <div id="url">
           <FontAwesomeIcon icon={faGlobeAmericas} />
           <a href={this.props.url} id="urla">{this.props.url}</a>
@@ -90,6 +91,10 @@ class Quote extends Component {
  * Loads a quote and renders the <Quote/>.
  */
 class QuoteLoader extends Component {
+  static randomItem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
   constructor(props) {
     super(props);
 
@@ -101,12 +106,10 @@ class QuoteLoader extends Component {
     };
   }
 
-  randomItem(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-  }
-
   componentDidMount() {
-    loadQuotes().then(quotes => this.setState(this.randomItem(quotes)));
+    loadQuotes().then((quotes) => {
+      this.setState(this.constructor.randomItem(quotes));
+    });
   }
 
   render() {
@@ -123,7 +126,7 @@ class QuoteLoader extends Component {
   }
 }
 
-(async function () {
+(async function main() {
   const options = await loadOptions();
   document.documentElement.classList.add(options.theme);
   ReactDOM.render(<QuoteLoader />, document.body);
