e52ff35afffb05a44b95a113aee897335ce6c29c
John Cipriano <johnmikecip@gmail.com>
2019-02-28 02:19:03 -0800

    trim at word boundaries in summarize

--- src/util.js
+++ src/util.js
@@ -87,11 +87,35 @@ export async function loadQuotes(stop) {
 
 /*
  * Returns a brief summary of text for use with the alert.
+ *
+ * Trims at word boundaries & adds an ellipsis at the end if needed.
+ *
+ * Tries to make it look better by removing some leading and
+ * trailing punctuation.
  */
-export const summarize = (text) => {
-  const len = 20
-  if (text.length < len) {
-    return text
-  }
-  return `(“${text.substring(0, len)}…”)`
+export const summarize = (text, len = 5) => {
+  /* eslint-disable no-multi-spaces */
+  const words = (
+    text
+      .replace(/["“”]/g, '') // remove quotation marks
+      .split(/\s+/)          // split on whitespace
+  )
+
+  const end = words.length - 1
+  words[0] = words[0].replace(/^\W+/g, '')     // no leading nonword chars
+  words[end] = words[end].replace(/\W+$/g, '') // no trailing nonword chars
+
+  const trimmed = (
+    words
+      .slice(0, len)        // take first n words
+      .join(' ')            // join string
+      .substring(0, 200)    // make sure it's short
+  )
+
+  const core = words.length <= len
+    ? trimmed
+    : `${trimmed}…`
+
+  return `“${core}”`
+  /* eslint-enable no-multi-spaces */
 }
